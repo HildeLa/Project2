0b67ebbaf1f8a4d76dfa79895a515639b690b087
HildeLa <hilde.langengen@gmail.com>
2022-10-27 15:18:59 +0200

    Update taskA_scikit.py

--- taskA_scikit.py
+++ taskA_scikit.py
@@ -8,6 +8,10 @@ from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 import random
 import numpy as np
+import pandas as pd
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 seed = np.random.seed(12345)
 
@@ -43,40 +47,60 @@ eta = 1.0/np.max(EigValues)
 learn_rates =  np.geomspace(0.0001, 0.1, 10, endpoint = True) # this is ineffective,
 epoch_range = np.geomspace(10, 1000, 10, endpoint = True)#but the thought is to be able to plot this
 
+plotting_dict = {'epoch_range':[],'learning_rate':[],'MSEtrain':[], 'MSEtest':[]}
 
 for rate in learn_rates: # looping through the learning rates I want to test
-    print(f'Results for learning rate {rate}:\n')
-    for range in epoch_range: # for each learning rate, I test each epoch lenght
+# or instead of doing this add learning_rate = 'adaptive' to model instead of specifying eta0
+    #print(f'Results for learning rate {rate}:\n')
+    for ep in epoch_range: # for each learning rate, I test each epoch lenght
         model = SGDRegressor(max_iter=10000,eta0=rate,fit_intercept=False) #Scikit learn Stocastic gradient descent regression
         #fit intercept is false because of the 1 in the feature matrix
         mse_train= [] # to be filled with train and test mses
         mse_test= [] #Planning to exchange with a matrix defined outside of loop for plotting all MSEs for each rate and epoch size
-        epochs = np.arange(1, range+1) #making a list for looping through
+        epochs = np.arange(1, ep+1) #making a list for looping through
+        plotting_dict['epoch_range'].append(ep)
+        plotting_dict['learning_rate'].append(rate)
         n_batch = 10 #I use 10 batches in each epoch
         batches = np.split(X_train, n_batch, axis=0) #Splitting the training data into the batches
         ybatches = np.split(y_train, n_batch, axis=0)
         batch_i= 0
-        for b, batch in enumerate(batches): #getting index to get the right y-batch
-            y_batch = ybatches[b]
-            try:
-                model.partial_fit(batch, y_batch)
-                batch_i += n_batch
-            except ValueError: #An error that occured, perhaps not anymore
-                print('for some reason y_batch is now ', len(y_batch))
-                print('while x-batch ', len(x_batch))
-                pass
+        for epoch in epochs:
+            for b, batch in enumerate(batches): #getting index to get the right y-batch
+                y_batch = ybatches[b]
+                try:
+                    model.partial_fit(batch, y_batch)
+                    batch_i += n_batch
+                except ValueError: #An error that occured, perhaps not anymore
+                    print('for some reason y_batch is now ', len(y_batch))
+                    print('while x-batch ', len(x_batch))
+                    pass
 
-            y_pred_test = model.predict(X_test)
-            y_pred_train = model.predict(X_train)
-            mse_train.append(MSE(y_train, y_pred_train))
-            mse_test.append(MSE(y_test, y_pred_test))
+                y_pred_test = model.predict(X_test)
+                y_pred_train = model.predict(X_train)
 
-        print('Coefficients: ', model.coef_) #printing coefficients for each epoch epoch_range
-        #Now, there is something weird
-    #fig, ax = plt.subplots()
-    #ax.plot(epochs, mse_test, label = 'mse test')
-    #ax.plot(epochs, mse_train, label = 'mse train')
-    #plt.xlabel('epoch')
-    #plt.ylabel('MSE scores')
-    #plt.legend()
-    #plt.show()
+                mse_train.append(MSE(y_train, y_pred_train))
+                mse_test.append(MSE(y_test, y_pred_test))
+        plotting_dict['MSEtrain'].append(MSE(y_train, y_pred_train))
+        plotting_dict['MSEtest'].append(MSE(y_test, y_pred_test))
+        #print('Coefficients: ', model.coef_) #printing coefficients for each epoch epoch_range
+df = pd.DataFrame(plotting_dict)
+print(df)
+
+#Plotting
+'''
+plot_X, plot_y = np.meshgrid(learn_rates, epoch_range)
+A = np.array(plotting_dict['MSEtest'])
+plot_z = np.reshape(A, (-1, 10))
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_zlabel('MSE score')
+
+surf = ax.plot_surface(plot_X, plot_y, plot_z,
+    cmap=cm.coolwarm,linewidth=0, antialiased=False)
+
+plt.xlabel('Learning rate')
+plt.ylabel('epoch lenghts')
+
+plt.show()
+'''
